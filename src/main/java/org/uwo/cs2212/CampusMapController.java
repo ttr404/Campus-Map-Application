b846579c93d338a50bf5b7e062ecf0c1076d5402
Yaopeng Xie <yxie447@uwo.ca>
2023-03-29 22:25:19 -0400

    fix layer bug

--- src/main/java/org/uwo/cs2212/CampusMapController.java
+++ src/main/java/org/uwo/cs2212/CampusMapController.java
@@ -149,7 +149,7 @@ public class CampusMapController implements Initializable {
         editButton.setVisible(isAdmin);
     }
 
-    private void filter(){
+    private void checkBoxSelected(){
         informationList.getItems().clear();
         for (Layer layer: currentFloorMap.getLayers()){
             for(PointOfInterest poi : layer.getPoints()){
@@ -162,136 +162,64 @@ public class CampusMapController implements Initializable {
                         || (cs_Labs.isSelected() && poi.getType().toLowerCase().equals("cs_labs"))
                         || (collaborative.isSelected() && poi.getType().toLowerCase().equals("collaborative"))
                 ){
-                    informationList.getItems().add(new SearchResult(currentFloorMap, poi));
-                }
-            }
-        }
-
-    }
-
-    public void onClassrooms(ActionEvent actionEvent){
-        filter();
-        for (Layer layer: currentFloorMap.getLayers()){
-            for(PointOfInterest poi : layer.getPoints()){
-                if ((!classrooms.isSelected())&& poi.getType().toLowerCase().equals("classroom")){
-                    layer.setHideLayer(true);
-                }
-                if ((classrooms.isSelected())&& poi.getType().toLowerCase().equals("classroom")){
                     layer.setHideLayer(false);
+                    informationList.getItems().add(new SearchResult(currentFloorMap, poi));
+
+                }
+                if ((!classrooms.isSelected() && poi.getType().toLowerCase().equals("classroom"))
+                        || (!stairwells.isSelected() && poi.getType().toLowerCase().equals("stairwell"))
+                        || (!elevators.isSelected() && poi.getType().toLowerCase().equals("elevator"))
+                        || (!entryAndExit.isSelected() && poi.getType().toLowerCase().equals("entryAndExit"))
+                        || (!genlabs.isSelected() && poi.getType().toLowerCase().equals("genlab"))
+                        || (!restaurants.isSelected() && poi.getType().toLowerCase().equals("restaurant"))
+                        || (!cs_Labs.isSelected() && poi.getType().toLowerCase().equals("cs_labs"))
+                        || (!collaborative.isSelected() && poi.getType().toLowerCase().equals("collaborative"))
+                ){
+                    layer.setHideLayer(true);
                 }
             }
         }
         showMap();
+    }
+
+
+
+    public void onClassrooms(ActionEvent actionEvent){
+        checkBoxSelected();
     }
 
     public void onStairwells(ActionEvent actionEvent){
-        filter();
-        for (Layer layer: currentFloorMap.getLayers()){
-            for(PointOfInterest poi : layer.getPoints()){
-                if ((!stairwells.isSelected())&& poi.getType().toLowerCase().equals("stairwell")){
-                    layer.setHideLayer(true);
-                }
-                if ((stairwells.isSelected())&& poi.getType().toLowerCase().equals("stairwell")){
-                    layer.setHideLayer(false);
-                }
-            }
-        }
-        showMap();
+        checkBoxSelected();
     }
 
     public void onElevators(ActionEvent actionEvent){
-        filter();
-        for (Layer layer: currentFloorMap.getLayers()){
-            for(PointOfInterest poi : layer.getPoints()){
-                if ((!elevators.isSelected())&& poi.getType().toLowerCase().equals("elevator")){
-                    layer.setHideLayer(true);
-                }
-                if ((elevators.isSelected())&& poi.getType().toLowerCase().equals("elevator")){
-                    layer.setHideLayer(false);
-                }
-            }
-        }
-        showMap();
+        checkBoxSelected();
     }
 
 
     public void onEntryAndExit(ActionEvent actionEvent){
-        for (Layer layer: currentFloorMap.getLayers()){
-            for(PointOfInterest poi : layer.getPoints()){
-                if ((!entryAndExit.isSelected())&& poi.getType().toLowerCase().equals("entryAndExit")){
-                    layer.setHideLayer(true);
-                }
-                if ((entryAndExit.isSelected())&& poi.getType().toLowerCase().equals("entryAndExit")){
-                    layer.setHideLayer(false);
-                }
-            }
-        }
-        showMap();
-        filter();
+        checkBoxSelected();
     }
 
     public void onGenlabs(ActionEvent actionEvent){
-        for (Layer layer: currentFloorMap.getLayers()){
-            for(PointOfInterest poi : layer.getPoints()){
-                if ((!genlabs.isSelected())&& poi.getType().toLowerCase().equals("genlab")){
-                    layer.setHideLayer(true);
-                }
-                if ((genlabs.isSelected())&& poi.getType().toLowerCase().equals("genlab")){
-                    layer.setHideLayer(false);
-                }
-            }
-        }
-        showMap();
-        filter();
+        checkBoxSelected();
     }
 
     public void onRestaurants(ActionEvent actionEvent){
-        for (Layer layer: currentFloorMap.getLayers()){
-            for(PointOfInterest poi : layer.getPoints()){
-                if ((!restaurants.isSelected())&& poi.getType().toLowerCase().equals("restaurant")){
-                    layer.setHideLayer(true);
-                }
-                if ((restaurants.isSelected())&& poi.getType().toLowerCase().equals("restaurant")){
-                    layer.setHideLayer(false);
-                }
-            }
-        }
-        showMap();
-        filter();
+        checkBoxSelected();
     }
 
     public void onCS_Labs(ActionEvent actionEvent){
-        for (Layer layer: currentFloorMap.getLayers()){
-            for(PointOfInterest poi : layer.getPoints()){
-                if ((!cs_Labs.isSelected())&& poi.getType().toLowerCase().equals("cs_labs")){
-                    layer.setHideLayer(true);
-                }
-                if ((cs_Labs.isSelected())&& poi.getType().toLowerCase().equals("cs_labs")){
-                    layer.setHideLayer(false);
-                }
-            }
-        }
-        showMap();
-        filter();
+        checkBoxSelected();
     }
 
     public void onCollaborative(ActionEvent actionEvent){
-        for (Layer layer: currentFloorMap.getLayers()){
-            for(PointOfInterest poi : layer.getPoints()){
-                if ((!collaborative.isSelected())&& poi.getType().toLowerCase().equals("collaborative")){
-                    layer.setHideLayer(true);
-                }
-                if ((collaborative.isSelected())&& poi.getType().toLowerCase().equals("collaborative")){
-                    layer.setHideLayer(false);
-                }
-            }
-        }
-        showMap();
-        filter();
+        checkBoxSelected();
     }
 
     private void setShowAllPOI(){
         selectAllLayers();
+        checkBoxSelected();
         informationList.getItems().clear();
         for (Layer layer: currentFloorMap.getLayers()){
             for(PointOfInterest poi : layer.getPoints()){
@@ -737,6 +665,7 @@ public class CampusMapController implements Initializable {
 
     public void onSearchButtonClicked(ActionEvent actionEvent) {
         selectAllLayers();
+        checkBoxSelected();
         String text = searchText.getText().toLowerCase().trim();
         if (!text.equals("")){
             informationList.getItems().clear();
@@ -788,6 +717,7 @@ public class CampusMapController implements Initializable {
 
     public void onClearButtonClicked(ActionEvent actionEvent) {
         deselectAllLayers();
+        checkBoxSelected();
         searchText.setText("");
         informationList.getItems().clear();
     }
